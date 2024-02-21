aed8cdac94b4a501faa2d961af20cccb2631bfbf
Saurav Paintola <sauravpantola101@gmail.com>
2024-02-21 13:24:22 +0530

    Firebase Push Notification Functionality added

--- App.tsx
+++ App.tsx
@@ -1,10 +1,31 @@
 /* eslint-disable prettier/prettier */
-import React from 'react';
-import {View} from 'react-native';
-import {WebView} from 'react-native-webview';
+import React, { useEffect } from 'react';
+import { Alert, View } from 'react-native';
+import { WebView } from 'react-native-webview';
+import messaging from '@react-native-firebase/messaging';
+import { PermissionsAndroid } from 'react-native';
+PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS);
+
+
+const getToken = async () => {
+  const token = await messaging().getToken()
+  console.log(token)
+}
+
+
 function App(): React.JSX.Element {
+  useEffect(() => { getToken() })
+  useEffect(() => {
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+      console.log('A new FCM message arrived!', JSON.stringify(remoteMessage));
+    });
+
+    return unsubscribe;
+  }, []);
+
   return (
-    <View style={{flex: 1, alignItems: 'stretch'}}>
+    <View style={{ flex: 1, alignItems: 'stretch' }}>
       <WebView
         source={{
           uri: 'https://www.widbazaar.com/',
